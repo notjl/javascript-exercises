5e1498275272ae0323271da13cbecb5228f647bc
notjl <agcabezas@protonmail.com>
2023-09-13 21:39:07 +0800

    Finished 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let ret_str = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        ret_str += str;
+    }
+    return ret_str;
 };
 
 // Do not edit below this line
