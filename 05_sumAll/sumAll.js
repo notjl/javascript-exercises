e7d8e22bb705f801bb729507950ba2ba3e3a2be1
notjl <agcabezas@protonmail.com>
2023-09-13 23:07:29 +0800

    Finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    if (a > b) {
+        [b, a] = [a, b];
+    }
+    for (; a <= b; a++) {
+        sum += a;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
