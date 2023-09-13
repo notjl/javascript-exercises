72bef16f67b2ff12b361165432649a1f87a7acab
notjl <agcabezas@protonmail.com>
2023-09-13 22:29:26 +0800

    Finished 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let ret_str = "";
+    let length = str.length
+    for (let i = 1; i <= length; i++) {
+        ret_str += str.at(0 - i)
+    }
 
+    /*
+     * Intended algo is O(1)
+     * return str.split("").reverse().join("")
+     */
+
+    // return ret_str
+    return str.split("").reverse().join("")
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toDelete) {
+    // Checks whether the element is included in the toDelete array
+    // If it is, then it will be filtered.
+    return arr.filter(elem => !toDelete.includes(elem))
 };
 
 // Do not edit below this line
